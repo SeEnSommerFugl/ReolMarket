886339d9cfb1f76819a8eea5209518ad3e2f1090
Patrick Kokholm <pako71406@edu.ucl.dk>
2025-09-05 13:38:33 +0200

    Initial Push

---
+++ ReolMarket/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,11 @@
+﻿using ReolMarket.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReolMarket.MVVM.ViewModel {
+    public class MainViewModel : ObservableObject {
+    }
+}
