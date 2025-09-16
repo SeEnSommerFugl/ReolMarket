5826aa87cf4bb25dcd253088f5622319ca37013c
Patrick Kokholm <pako71406@edu.ucl.dk>
2025-09-16 13:23:37 +0200

    Implementeret scenarie 3 klasser

---
+++ ReolMarket/MVVM/Model/Sale.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReolMarket.MVVM.Model
+{
+    class Sale
+    {
+        public Guid SaleID { get; set; } = Guid.NewGuid();
+        public Guid ShoppingCartID { get; set; }
+        public Guid PaymentID { get; set; }
+    }
+}
