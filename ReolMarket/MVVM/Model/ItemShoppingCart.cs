f8cb5445a47223a71a49479914168fc17f0c6cae
WildclawsDK <68854789+WildclawsDK@users.noreply.github.com>
2025-09-17 14:57:59 +0200

    Refactor repository classes and enhance SQL commands
    Added `sealed` modifier to multiple repository classes to prevent inheritance. Updated `ItemShoppingCartDbRepository` and others with new SQL commands for CRUD operations. Modified `ItemShoppingCart` to include a record struct for improved key handling.

--- ReolMarket/MVVM/Model/ItemShoppingCart.cs
+++ ReolMarket/MVVM/Model/ItemShoppingCart.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ReolMarket.MVVM.Model
+﻿namespace ReolMarket.MVVM.Model
 {
     class ItemShoppingCart
     {
         public Guid ItemID { get; set; }
         public Guid ShoppingCartID { get; set; }
+        public readonly record struct ItemShoppingCartKey(Guid ItemId, Guid CartId);
     }
 }
