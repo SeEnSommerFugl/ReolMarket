d9f5c5ac1d0824e8d780842a696e2fbd542c785c
Patrick Kokholm <54838826+Paztrix@users.noreply.github.com>
2025-09-22 13:53:46 +0200

    Update ShoppingCart.cs

--- ReolMarket/MVVM/Model/ShoppingCart.cs
+++ ReolMarket/MVVM/Model/ShoppingCart.cs
@@ -3,7 +3,6 @@
     public class ShoppingCart
     {
         public Guid ShoppingCartId { get; set; } = Guid.NewGuid();
-        public int Quantity { get; set; }
         public decimal TotalPrice { get; set; }
     }
 }
