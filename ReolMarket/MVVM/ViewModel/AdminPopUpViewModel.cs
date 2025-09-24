c98edd95c6afdc0227c3702713e37e49af5ed0d6
WildclawsDK <68854789+WildclawsDK@users.noreply.github.com>
2025-09-24 12:57:35 +0200

    Add admin popup functionality and navigation commands
    Updated `RentersView.xaml` to bind a button to `NavigateAdminPopUpCommand` for dynamic navigation. Introduced `NavigateRentersCommand` in `MainViewModel.cs` for navigating to the `RentersViewModel`. Added `NavigateAdminPopUpCommand` in `RentersViewModel.cs` to open the admin popup. Created `AdminPopUpViewModel.cs`, `AdminPopUpView.xaml`, and `AdminPopUpView.xaml.cs` to define the admin popup's layout and functionality.

---
+++ ReolMarket/MVVM/ViewModel/AdminPopUpViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ReolMarket.Core;
+using ReolMarket.Data;
+using ReolMarket.MVVM.Model;
+
+namespace ReolMarket.MVVM.ViewModel
+{
+    internal class AdminPopUpViewModel : BaseViewModel
+    {
+        private IBaseRepository<Booth, Guid> _boothRepo;
+        private IBaseRepository<Customer, Guid> _customerRepo;
+
+        public AdminPopUpViewModel(IBaseRepository<Booth, Guid> boothRepo, IBaseRepository<Customer, Guid> customerRepo)
+        {
+            _boothRepo = boothRepo;
+            _customerRepo = customerRepo;
+        }
+    }
+}
