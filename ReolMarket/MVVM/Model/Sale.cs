8148d6a1385f118f35b8d4e971325599df54c10e
Patrick Kokholm <54838826+Paztrix@users.noreply.github.com>
2025-09-24 11:25:30 +0200

    Update Sale.cs

--- ReolMarket/MVVM/Model/Sale.cs
+++ ReolMarket/MVVM/Model/Sale.cs
@@ -5,5 +5,6 @@
         public Guid SaleID { get; set; } = Guid.NewGuid();
         public Guid ShoppingCartID { get; set; }
         public Guid PaymentID { get; set; }
+        public DateTime SaleDate { get; set; }
     }
 }
