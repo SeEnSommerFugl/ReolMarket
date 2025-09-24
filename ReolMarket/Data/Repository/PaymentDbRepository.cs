946c516e6dbd67a5342f5ff2c9a5235031ca1894
WildclawsDK <68854789+WildclawsDK@users.noreply.github.com>
2025-09-24 11:28:21 +0200

    Remove PaymentDate field from PaymentDbRepository
    The `PaymentDbRepository` class has been updated to eliminate all references to the `PaymentDate` field. This includes modifications to the `SELECT`, `INSERT`, and `UPDATE` SQL statements, as well as the removal of `PaymentDate` parameters in the `BindInsert` and `BindUpdate` methods. This change reflects an update in the data model where `PaymentDate` is no longer utilized or stored in the database.

--- ReolMarket/Data/Repository/PaymentDbRepository.cs
+++ ReolMarket/Data/Repository/PaymentDbRepository.cs
@@ -7,22 +7,21 @@ namespace ReolMarket.Data.Repository
     internal sealed class PaymentDbRepository : BaseDbRepository<Payment, Guid>
     {
         protected override string SqlSelectAll => @"
-        SELECT PaymentID, PaymentMethod, PaymentDate
+        SELECT PaymentID, PaymentMethod
         FROM Payment";
 
         protected override string SqlSelectById => @"
-        SELECT PaymentID, PaymentMethod, PaymentDate
+        SELECT PaymentID, PaymentMethod
         FROM Payment
         WHERE PaymentID = @PaymentID";
 
         protected override string SqlInsert => @"
-        INSERT INTO Payment (PaymentID, PaymentMethod, PaymentDate)
-        VALUES (@PaymentID, @PaymentMethod, @PaymentDate);";
+        INSERT INTO Payment (PaymentID, PaymentMethod)
+        VALUES (@PaymentID, @PaymentMethod);";
 
         protected override string SqlUpdate => @"
         UPDATE Payment
            SET PaymentMethod = @PaymentMethod,
-               PaymentDate   = @PaymentDate
          WHERE PaymentID     = @PaymentID;";
 
         protected override string SqlDeleteById => @"
@@ -32,8 +31,7 @@ namespace ReolMarket.Data.Repository
         protected override Payment Map(IDataRecord r) => new Payment
         {
             PaymentID = r.GetGuid(r.GetOrdinal("PaymentID")),
-            PaymentMethod = r.GetString(r.GetOrdinal("PaymentMethod")),
-            PaymentDate = r.GetDateTime(r.GetOrdinal("PaymentDate"))
+            PaymentMethod = r.GetString(r.GetOrdinal("PaymentMethod"))
         };
 
         protected override void BindId(SqlCommand cmd, Guid id)
@@ -45,13 +43,11 @@ namespace ReolMarket.Data.Repository
         {
             cmd.Parameters.Add("@PaymentID", SqlDbType.UniqueIdentifier).Value = e.PaymentID;
             cmd.Parameters.Add("@PaymentMethod", SqlDbType.NVarChar, 255).Value = e.PaymentMethod ?? (object)DBNull.Value;
-            cmd.Parameters.Add("@PaymentDate", SqlDbType.DateTime2).Value = e.PaymentDate;
         }
 
         protected override void BindUpdate(SqlCommand cmd, Payment e)
         {
             cmd.Parameters.Add("@PaymentMethod", SqlDbType.NVarChar, 255).Value = e.PaymentMethod ?? (object)DBNull.Value;
-            cmd.Parameters.Add("@PaymentDate", SqlDbType.DateTime2).Value = e.PaymentDate;
             cmd.Parameters.Add("@PaymentID", SqlDbType.UniqueIdentifier).Value = e.PaymentID;
         }
 
