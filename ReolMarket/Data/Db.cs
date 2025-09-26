cf404af5bef541805854643312d18bb0287fc901
Outlashed <Outlashed@Gmail.com>
2025-09-26 13:16:00 +0200

    Will move your database access from local, to Remote - Requires functional appsettings.Local.json

--- ReolMarket/Data/Db.cs
+++ ReolMarket/Data/Db.cs
@@ -8,11 +8,14 @@ namespace ReolMarket.Data
         /// <summary>
         /// Config builder for connection string, gets the "appsettings.json", with optional to false; it must be provided.
         /// "reloadOnChange" to false, means, no hot reload when changing the connection string.
+        /// appsettings.json contains no secrets
+        /// appsettings.Local.json contains secrets, but is in .gitignore so nobodys logins are shared.
         /// </summary>
         private static readonly IConfigurationRoot _config =
             new ConfigurationBuilder()
                 .SetBasePath(AppContext.BaseDirectory)
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
+                .AddJsonFile("appsettings.Local.json", optional: true, reloadOnChange: true)
                 .AddEnvironmentVariables()
                 .Build();
         /// <summary>
