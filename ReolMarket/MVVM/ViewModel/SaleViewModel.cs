24fde1f425942a069c170d1d900049a80a33ffb5
Patrick Kokholm <patrickkokholm@gmail.com>
2025-10-01 15:31:37 +0200

    Startet med at implementere saleviewmodel

--- ReolMarket/MVVM/ViewModel/SaleViewModel.cs
+++ ReolMarket/MVVM/ViewModel/SaleViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Net.Http.Headers;
 using System.Windows.Input;
 using ReolMarket.Core;
 using ReolMarket.Data;
@@ -14,13 +15,41 @@ namespace ReolMarket.MVVM.ViewModel
     {
         private readonly IBaseRepository<Sale, Guid> _saleRepo;
         private readonly IBaseRepository<Booth, Guid> _boothRepo;
-        private readonly IBaseRepository<Customer, Guid> _customerRepo;
+        private readonly IBaseRepository<Item, Guid> _itemRepo;
 
         public ObservableCollection<Sale> Sales => _saleRepo.Items;
         public ObservableCollection<Booth> Booths => _boothRepo.Items;
-        public ObservableCollection<Customer> Customers => _customerRepo.Items;
-
         public ICollectionView SaleView {  get; }
+
+        private string? _itemName;
+        public string? ItemName
+        {
+            get => _itemName;
+            set 
+            {
+                SetProperty(ref _itemName, value);
+            }
+        }
+
+        private decimal? _itemPrice;
+        public decimal? ItemPrice 
+        {
+            get => _itemPrice;
+            set 
+            {
+                SetProperty(ref _itemPrice, value);
+            }
+        }
+
+        private int? _boothNumber;
+        public int? BoothNumber 
+        {
+            get => _boothNumber;
+            set 
+            {
+                SetProperty(ref _boothNumber, value);
+            }
+        }
 
         /// <summary>
         /// Start date for the sale period.
@@ -44,13 +73,54 @@ namespace ReolMarket.MVVM.ViewModel
             set => SetProperty(ref _periodEnd, value);
         }
 
-        public SaleViewModel(IBaseRepository<Booth, Guid> boothRepo, IBaseRepository<Customer, Guid> customerRepo, IBaseRepository<Sale, Guid> saleRepo)
+        public ICommand RegisterSaleCommand { get; }
+
+        public SaleViewModel(IBaseRepository<Booth, Guid> boothRepo, IBaseRepository<Sale, Guid> saleRepo, IBaseRepository<Item, Guid> itemRepo)
         {
             _boothRepo = boothRepo;
-            _customerRepo = customerRepo;
             _saleRepo = saleRepo;
+            _itemRepo = itemRepo;
 
-            //ExecuteGenerate();
+            RegisterSaleCommand = new RelayCommand(_ => RegisterSale(), _ => CanRegisterSale());
+        }
+
+        private void RegisterSale() 
+        {
+            var booth = Booths.FirstOrDefault(b => b.BoothNumber == BoothNumber);
+
+            var newItem = new Item 
+            {
+                ItemName = ItemName,
+                ItemPrice = (decimal)ItemPrice,
+                BoothID = booth.BoothID,
+                Booth = booth
+            };
+            _itemRepo.Add(newItem);
+
+            var cart = new ShoppingCart();
+            var cartItem = new ItemShoppingCart 
+            {
+                ItemID = newItem.ItemID,
+                ShoppingCartID = cart.ShoppingCartId
+            };
+
+            var sale = new Sale {
+                SaleDate = DateTime.Now,
+                ShoppingCartID = cart.ShoppingCartId,
+                PaymentID = Guid.NewGuid() //TODO Her skal laves en enum til kort og kontant
+            };
+            _saleRepo.Add(sale);
+
+            ItemName = string.Empty;
+            ItemPrice = 0;
+            BoothNumber = null;
+        }
+
+        private bool CanRegisterSale() 
+        {
+            return !string.IsNullOrWhiteSpace(ItemName)
+                && ItemPrice > 0
+                && BoothNumber.HasValue;
         }
     }
 }
